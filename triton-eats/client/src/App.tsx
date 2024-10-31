8f9aefe7d2578257a9aaefa4f3996c7881f0037b
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-10-31 00:45:56 -0700

    Started on Recommendations, images element done

--- triton-eats/client/src/App.tsx
+++ triton-eats/client/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import './CSS/main.css';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { Recommend } from './pages/Recommend';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/*HomePage*/ }
+      
+        {/*Recommendation Page*/ }
+      <Route path="/recommend" element={<Recommend />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ triton-eats/client/src/components/RecommendFilter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const RecommendFilter = () => {
+   
+    return (
+        <div className='RecommendFilter'>
+
+        </div>
+    );
+
+    
+}
+
+export default RecommendFilter;
