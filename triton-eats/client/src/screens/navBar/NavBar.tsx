fa7a5bf3073e2775ecb86ee70449f12200d5c955
fyash1010 <fyash@ucsd.edu>
2024-10-30 22:21:45 -0700

    Initial navBar deisgn

---
+++ triton-eats/client/src/screens/navBar/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+import logo from './logo.png';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={ logo } alt="Triton Eats Logo" className="logo-icon" />
+      </div>
+      <ul className="navbar-links">
+        <li className="navbar-item">Homepage</li>
+        <li className="navbar-item selected">Reviews</li>
+        <li className="navbar-item">Capacity</li>
+        <li className="navbar-item">Recommendations</li>
+        <li className="navbar-item">Recipes</li>
+      </ul>
+      <div className="navbar-auth">
+        <button className="signin-btn">Sign in</button>
+        <button className="register-btn">Register</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
