29971472492148ccfe746f5fb840cc29a996e0cc
fyash1010 <fyash@ucsd.edu>
2024-11-08 09:32:13 -0800

    Added tests checking if the navigation buttons on the navBar actually take user to their respective pages. Need to add tests checking if the navBar shows the current page as 'selected' (white border around the button).

--- triton-eats/client/src/App.test.tsx
+++ triton-eats/client/src/App.test.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+describe('navBar navigations', () => {
+  test('navigate to homepage', () => {
+    render(<App />);
 
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+    const menuLink = screen.getByRole('link', { name: 'Homepage' });
+    fireEvent.click(menuLink);
 
+    expect(window.location.pathname).toBe('/');
+  });
+
+  test('navigate to menu', () => {
+    render(<App />);
+
+    const menuLink = screen.getByRole('link', { name: 'Menu' });
+    fireEvent.click(menuLink);
+
+    expect(window.location.pathname).toBe('/menu');
+  });
+
+  test('navigate to reviews', () => {
+    render(<App />);
+
+    const menuLink = screen.getByRole('link', { name: 'Reviews' });
+    fireEvent.click(menuLink);
+
+    expect(window.location.pathname).toBe('/review');
+  });
 });
