8c4994513302fde0f5d6f7803c8ebbcdcc9a536c
Eric Zheng <114521575+erizheng@users.noreply.github.com>
2024-11-12 22:00:16 -0800

    Finished Sort Buttons

--- triton-eats/client/src/components/Menu/MenuSortButtons.tsx
+++ triton-eats/client/src/components/Menu/MenuSortButtons.tsx
@@ -7,6 +7,9 @@ import { sortDishes, fetchDishes } from '../../utils/menu-utils';
 
 export const MenuSortButtons = () => {
     const { dishes, setDishes } = useContext(MenuContext);
+    const [clickCountCost, setClickCountCost] = useState(0);
+    const [clickCountName, setClickCountName] = useState(0);
+    const [clickCountRate, setClickCountRate] = useState(0);
 
     useEffect(() => {
         loadMenu();
@@ -23,69 +26,80 @@ export const MenuSortButtons = () => {
     };
 
     //COST SORTING
-    const handleCostDescend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => b.cost - a.cost); // Sort in decending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
-    };
-    const handleCostAscend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => a.cost - b.cost); // Sort in ascending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
+    const handleCost = () => {
+        setClickCountCost(clickCountCost + 1);
+        setClickCountName(0);
+        setClickCountRate(0);
+        if (clickCountCost % 2 === 0) {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                a.cost - b.cost); // Sort in ascending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        } else {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                b.cost - a.cost); // Sort in decending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        }
     };
 
     //RATING SORTING
-    const handleRateAscend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => a.rating - b.rating); // Sort in ascending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
-    };
-    const handleRateDescend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => b.rating - a.rating); // Sort in decending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
+    const handleRate = () => {
+        setClickCountCost(0);
+        setClickCountName(0);
+        setClickCountRate(clickCountRate + 1);
+        if (clickCountRate % 2 === 0) {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                a.rating - b.rating); // Sort in ascending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        } else {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                b.rating - a.rating); // Sort in decending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        }
     };
 
-    //DISTANCE SORT
     //NAME SORT
-    const handleNameAscend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => 
-            a.food_name.localeCompare(b.food_name)); // Sort in ascending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
+    const handleName = () => {
+        setClickCountCost(0);
+        setClickCountName(clickCountName + 1);
+        setClickCountRate(0);
+        if (clickCountName % 2 === 0) {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                a.food_name.localeCompare(b.food_name)); // Sort in ascending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        } else {
+            const sortedListByCost = [...dishes].sort((a, b) => 
+                b.food_name.localeCompare(a.food_name)); // Sort in descending order;
+            sortDishes(sortedListByCost);
+            setDishes(sortedListByCost);
+        }
+        
     };
-    const handleNameDescend = () => {
-        const sortedListByCost = [...dishes].sort((a, b) => 
-            b.food_name.localeCompare(a.food_name)); // Sort in descending order;
-        sortDishes(sortedListByCost);
-        setDishes(sortedListByCost);
-    };
+    //DISTANCE SORT
+    
     
 
     return (
         <div className='sortFilter'>
             <ul className="navbar-links">
                 {/* <li className="sortItem">Distance</li>
-                <li className="sortItem">Ratings</li>
                 <li className="sortItem">Busyness</li> */}
                 <button className="sortItem" onClick={() => {
-                    handleCostAscend()
-                }}>Price ^</button>{/* Sorts list ascend */}
+                    handleCost()
+                }}>Price</button>{/* Sorts list ascend */}
+
                 <button className="sortItem" onClick={() => {
-                    handleCostDescend()
-                }}>Price Down</button>{/* Sorts list Descend */}
+                    handleRate()
+                }}>Rate</button>{/* Sorts list ascend */}
+                
                 <button className="sortItem" onClick={() => {
-                    handleRateAscend()
-                }}>Rate ^</button>{/* Sorts list ascend */}
-                <button className="sortItem" onClick={() => {
-                    handleRateDescend()
-                }}>Rate Down</button>{/* Sorts list descend */}
-                <button className="sortItem" onClick={() => {
-                    handleNameAscend()
-                }}>Name ^</button>{/* Sorts list ascend */}
-                <button className="sortItem" onClick={() => {
-                    handleNameDescend()
-                }}>Name Down</button>{/* Sorts list ascend */}
+                    handleName()
+                }}>Name</button>{/* Sorts list ascend */}
+                
             </ul>
                     
         </div>
