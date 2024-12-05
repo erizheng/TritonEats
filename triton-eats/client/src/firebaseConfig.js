54ddd8d4215140e80b828566563f5a55ab030e53
vaishnavi-rama <vramanujan@ucsd.edu>
2024-12-04 23:01:07 -0800

    preference tests

--- triton-eats/client/src/firebaseConfig.js
+++ triton-eats/client/src/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAeum9QrymFNfs8SCCXRygLXYo_Vd2ZS_Y",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: "tritoneats-ce4d9.firebaseapp.com",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
